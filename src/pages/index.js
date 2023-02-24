8f1f827db2171b6f97c0311a6f26a64b75c76b1b
Dominik Orincsai <o.dominik2107@gmail.com>
2023-02-25 00:42:42 +0100

    feat(home): hero section wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const Home = () => {
       <main className="bg-white px-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">doshinxlsx</h1>
+            <h1 className="text-xl font-burtons">Dominik Orincsai</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill className="cursor-pointer text-xl" />
@@ -28,6 +28,17 @@ const Home = () => {
               </li>
             </ul>
           </nav>
+
+          <div>
+            <h2>Dominik Orincsai</h2>
+            <h3>Developer and Cybersecurity Enthusiast</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui amet
+              quia expedita distinctio nesciunt molestias odit vel voluptates,
+              consectetur itaque cumque repellat cum sit accusantium inventore
+              quasi quaerat architecto sunt.
+            </p>
+          </div>
         </section>
       </main>
     </div>
