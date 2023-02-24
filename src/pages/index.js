9e555623e056c9c83067c4f861c2f651f76d13ae
Dominik Orincsai <o.dominik2107@gmail.com>
2023-02-25 00:40:23 +0100

    feat(home): home wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <Head>
@@ -9,11 +10,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className="bg-blue-700 min-h-screen">
-          <h1>Hello</h1>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl">doshinxlsx</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
         </section>
       </main>
     </div>
   );
-}
+};
+
+export default Home;
