426a77a697a1768f6fd0f1dfa13188672845654a
Dominik Orincsai <o.dominik2107@gmail.com>
2023-02-25 03:45:29 +0100

    feat(home): added skills section

--- src/pages/index.js
+++ src/pages/index.js
@@ -98,6 +98,48 @@ const Home = () => {
               </li>
             </ul>
           </div>
+
+          <div className="mt-20">
+            <h3 className="text-2xl font-bold py-1 border-b-4 w-16 border-gray-400 dark:text-cyan-300">
+              Skills
+            </h3>
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+              <div className="grid grid-cols-2 row-gap-8 md:grid-cols-4">
+                <div className="text-center md:border-r">
+                  <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl dark:text-white">
+                    7/10
+                  </h6>
+                  <p className="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base dark:text-cyan-300">
+                    React
+                  </p>
+                </div>
+                <div className="text-center md:border-r">
+                  <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl dark:text-white">
+                    7/10
+                  </h6>
+                  <p className="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base dark:text-cyan-300">
+                    Javascript
+                  </p>
+                </div>
+                <div className="text-center md:border-r">
+                  <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl dark:text-white">
+                    6/10
+                  </h6>
+                  <p className="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base dark:text-cyan-300">
+                    Python
+                  </p>
+                </div>
+                <div className="text-center">
+                  <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl dark:text-white">
+                    7/10
+                  </h6>
+                  <p className="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base dark:text-cyan-300">
+                    Networking
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
 
         <section>
